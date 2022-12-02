a69418fb9709c93d3af1ac705b03c91df88b4672
Martin John M <111407805+martinjohnm@users.noreply.github.com>
2022-12-02 12:20:32 +0530

    Update settings.py
    reset path env

--- VEGIE_PROJECT/settings.py
+++ VEGIE_PROJECT/settings.py
@@ -20,7 +20,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, 'VEGIE_PROJECT/.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
